8a20494c4bccfc1f58bb2cdfea3d5b5f7b67a8dd
alif33 <alifhasan332@gmail.com>
2022-08-17 14:07:48 +0600

    ...

--- __lib__/helpers/Cookiehandler.js
+++ __lib__/helpers/Cookiehandler.js
@@ -2,12 +2,12 @@ import Cookies from 'universal-cookie';
 
 export const userAuthStatus = async()=>{
     const cookie = new Cookies();
-    const _info = await cookie.get('__u__');
-    if(_info){
+    const _u_ = await cookie.get('__u__');
+    if(_u_){
         return {
             isUser: true,
-            token: _info.token,
-            user: _info.user,
+            token: _u_.token,
+            user: _u_.user,
         }
     }else{
         return {
