141a859b30ed29a006585eb515b0c9d11d0617d7
alif33 <alifhasan332@gmail.com>
2022-08-17 21:29:49 +0600

    ...

--- pages/auth/password-reset-email/[mail].js
+++ pages/auth/password-reset-email/[mail].js
@@ -11,25 +11,30 @@ const PasswordResetEmail = () => {
         if(router.query.mail){
             console.log(router.query.mail);
         }else{
-            router.push('/404');
+            router.push('/');
+            
             // setEmail(cryptr.decrypt(router.query.mail));
         }
         // console.log(router.query?.mail);
     }, [])
 
-
-    return (
-        <AuthLayout>
-            <div className="container ps-1 pe-2 mt-2 mb-5 vh-100">
-                <div className="w-100 d-flex justify-content-between align-items-center">
-                    <div className="profile-text mt-3">
-                        <h3>Password reset email sent</h3>
-                        <p className="mt-2">We have just sent a password reset link to <b>{ cryptr.decrypt(router.query.mail) }</b>. Please double-check that this is the email associated with your account. If you do not receive an email within the next few minutes, this may not be the email address for your account.</p>
+    if(router.query.mail){
+        return (
+            <AuthLayout>
+                <div className="container ps-1 pe-2 mt-2 mb-5 vh-100">
+                    <div className="w-100 d-flex justify-content-between align-items-center">
+                        <div className="profile-text mt-3">
+                            <h3>Password reset email sent</h3>
+                            <p className="mt-2">We have just sent a password reset link to <b>{ cryptr.decrypt(router.query.mail) }</b>. Please double-check that this is the email associated with your account. If you do not receive an email within the next few minutes, this may not be the email address for your account.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </AuthLayout>
-    );
+            </AuthLayout>
+        );
+    }else{
+        <></>
+    }
+
 };
 
 export default PasswordResetEmail;
