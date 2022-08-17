52c54ac21b9ea39b5e2d1004d1d26a3139bbabc4
alif33 <alifhasan332@gmail.com>
2022-08-18 00:57:20 +0600

    ...

--- pages/auth/forget-password/index.js
+++ pages/auth/forget-password/index.js
@@ -16,7 +16,7 @@ const ForgetPassword = () => {
     const [disable, setDisable] = useState(false);
     const { register, reset, handleSubmit, formState: { errors } } = useForm();
     const onError = err => showErr(err);
-    const cookies = new Cookies();
+    // const cookies = new Cookies();
     const router = useRouter();
 
     const onSubmit = data => {
@@ -43,45 +43,51 @@ const ForgetPassword = () => {
 
     
     return (
-        <AuthLayout>
-            <div className={style._smd_login_card}>
-                <h3>Reset Your Password</h3>
+        // <AuthLayout>
+            <div>
+                <div className={style._smd_login_card}>
+                    <h3>Reset Your Password</h3>
+                </div>
+
+                <h6 className={`my-2 text-center ${style.forget_password_desc}`} >Enter your email adress below and we’ll send you a link with instructions</h6>
+
+                <div className={style._smd_login_card}>
+                    <form 
+                        className="w-100"
+                        onSubmit={handleSubmit(onSubmit, onError)}
+                    >
+
+                        <div className={style._smd_form_group}>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="Email Address"
+                                {...register("email",
+                                    {
+                                        required: 'Email is required.'
+                                    }
+                                )}
+                            />
+                        </div>
+
+                        <button className="bg-black text-center justify-content-center  mt-3 mb-4"> Submit</button>
+                    </form>
+                    {/* <div>
+                        <a className="mt-1 mb-2">
+                            <Link href="/auth/sign-in">
+                                Remember your password? Login
+                            </Link>
+                        </a>
+                    </div> */}
+                    
+                    {/*
+                    <h6 className={`my-1 text-center ${style.forget_password_desc}`}>
+                        <a><Link href="/auth/sign-in">Need Help | FAQ</Link></a>
+                    </h6> */}
+                </div>
             </div>
-
-            <h6 className={`my-2 text-center ${style.forget_password_desc}`} >Enter your email adress below and we’ll send you a link with instructions</h6>
-
-            <div className={style._smd_login_card}>
-                <form 
-                    className="w-100"
-                    onSubmit={handleSubmit(onSubmit, onError)}
-                >
-
-                    <div className={style._smd_form_group}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            id="email"
-                            type="email"
-                            placeholder="Email Address"
-                            {...register("email",
-                                {
-                                    required: 'Email is required.'
-                                }
-                            )}
-                        />
-                    </div>
-
-                    <button className="bg-black text-center justify-content-center  mt-3 mb-4"> Submit</button>
-                </form>
-                <a className="mt-1 mb-2">Remember your password?
-                    <Link href="/auth/sign-in">
-                        Login
-                    </Link>
-                </a>
-                <h6 className={`my-1 text-center ${style.forget_password_desc}`} >
-                    <Link href="/auth/sign-in">Need Help | FAQ</Link>
-                </h6>
-            </div>
-        </AuthLayout>
+        // </AuthLayout>
     );
 };
 
