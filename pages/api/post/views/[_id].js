4ab68c5fe884dda82b4b12b7e80e3e3c0b6354d7
alif33 <alifhasan332@gmail.com>
2022-09-01 22:52:42 +0600

    ..

---
+++ pages/api/post/views/[_id].js
@@ -0,0 +1,30 @@
+import db from '../../../../../utils/db';
+import Post from '../../../../../models/Post';
+
+handler.get(async (req, res) => {
+  const { _id } = req.query;
+    await db.connect();
+    Post.findOneAndUpdate(
+        { _id }, 
+        { $inc: {
+            "views": 1
+        } },
+        { returnOriginal: false },
+
+        async(err, post)=>{
+            await db.disconnect();
+            if(err){
+                return res.status(400).json({
+                    err,
+                    message: "Something went wrong",
+                });
+            }
+
+            if(post){
+                return res.status(201).json({
+                    success: true
+                });
+            }
+        }
+    )
+});
---
+++ pages/news/[slug].js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const Post = () => {
+
+    useEffect(()=>{
+
+    }, [])
+    return (
+        <>
+            <h1>This is the most important news</h1>
+        </>
+    )
+}
+
+export default Post;
