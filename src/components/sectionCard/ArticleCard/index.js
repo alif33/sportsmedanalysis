32a336014ca9ace2fa377cc50e8c630699c9d2aa
alif33 <alifhasan332@gmail.com>
2022-09-02 21:09:14 +0600

    ...

--- src/components/sectionCard/ArticleCard/index.js
+++ src/components/sectionCard/ArticleCard/index.js
@@ -5,9 +5,16 @@ import articleStyle from './Article.module.css';
 
 const ArticleCard = ({ index, post }) => {
     return (
-        <Link key={ index } href={`/news/${ post.title }/${ post._id }`}>
+        <Link key={ index } href={`/news/${ post.slug }/${ post._id }`}>
             <a className={`${articleStyle._smd_article_card} w-100 w-md-25 mx-0 mx-md-1`}>
-                <Image height="150" width="240" src={ post.image } alt={post.title} />
+                <Image 
+                    height="150" 
+                    width="240" 
+                    src={ post.image } 
+                    alt={post.title} 
+                    
+                    // placeholder="blur"
+                />
                 <h4>Article</h4>
             </a>
         </Link>
