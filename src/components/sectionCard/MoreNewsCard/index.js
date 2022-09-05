ee589a9660ec7b8fb71fe666e34fc191a20a3f84
alif33 <alifhasan332@gmail.com>
2022-09-05 20:07:32 +0600

    ...

--- src/components/sectionCard/MoreNewsCard/index.js
+++ src/components/sectionCard/MoreNewsCard/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 import style from './MoreNewsCard.module.css';
+import Link from "next/link";
 
-const MoreNewsCard = ({ img, title }) => {
+const MoreNewsCard = ({ _id, slug, title, img }) => {
     return (
         <div className={style.more_news_card} >
-            <a href="#!">
-                <Image height="162" width="200" src="/images/landing_page/nfl_six.png" alt="" />
-                <p className={style.nfl_card_Desc}>{title}</p>
-            </a>
+            <Link href={`/news/${ slug }/${ _id }`}>
+                <a>
+                    <Image height="162" width="200" src={ img } alt={ title } />
+                    <p className={style.nfl_card_Desc}>{title}</p>
+                </a>
+            </Link>
         </div>
     );
 };
