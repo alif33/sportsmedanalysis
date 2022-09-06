fd8dfca4a7cdede336921e12731432f9bf251b4f
alif33 <alifhasan332@gmail.com>
2022-09-06 23:34:44 +0600

    ...

--- pages/players.js
+++ pages/players.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Player from '../models/Player';
 import Layout from '../src/components/Layout';
 import TeamsFollowing from '../src/section/TeamsFollowing';
 
-const Players = () => {
+const Players = ({ players }) => {
     return (
         <Layout navheader={ true }>
             <div className="container-fluid2 mt-3">
@@ -16,3 +17,23 @@ const Players = () => {
 };
 
 export default Players;
+
+
+export async function getStaticProps(context) {
+
+    await db.connect();
+    const players = await Player.find({}, { _comments: 0 })
+        .sort()
+        .lean()
+        .limit(50);
+
+    await db.disconnect();
+
+
+    return {
+        props: {
+            players: JSON.stringify(players),
+        },
+        revalidate: 60,
+    };
+}
