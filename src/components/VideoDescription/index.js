169f1de1f97abaff64e5a78008ca4c3e267e0982
developerYeasin <yea13sin@gmail.com>
2022-09-10 01:38:39 +0600

    play btn

--- src/components/VideoDescription/index.js
+++ src/components/VideoDescription/index.js
@@ -7,11 +7,11 @@ const VideoDescription = () => {
         <div className={style.videoDescription}>
             <div className={style.description}>
                 <h2>Where Jags Defense Needs To Step Up This Season</h2>
+                <ViewsCard/>
                 <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
                     sint. Velit officia consequat duis enim velit mollit. Exercitation
                     veniam consequat sunt nostrud amet.</p> <br /><br />
             </div>
-            {/* <ViewsCard/> */}
         </div>
     );
 };
