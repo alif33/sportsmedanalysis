2b911f6766e8ff06b88de6e8c8b469fe272b597f
developerYeasin <yea13sin@gmail.com>
2022-09-10 02:24:45 +0600

    ajsldfkj;l

--- src/components/ViewsCard/index.js
+++ src/components/ViewsCard/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ClockIcon from '../svg/ClockIcon';
-import DownloadIcon from '../svg/DownloadIcon';
 import EyeIcon from '../svg/EyeIcon';
-import ShareIcon from '../svg/ShareIcon';
 import style from './ViewsCard.module.css';
+import { FacebookShareButton, LinkedinShareButton, PinterestShareButton, TwitterShareButton } from "react-share";
+import { SiFacebook, SiLinkedin, SiPinterest, SiTwitter } from 'react-icons/si';
 
 const ViewsCard = ({ watch }) => {
+
+
+
+
     return (
         <ul className={style.viewsCard}>
-            <li><EyeIcon />{ watch.views } views | Sep 10, 2022</li>
-            <li><ShareIcon /></li>
+            <li><EyeIcon />{watch.views} views | Sep 10, 2022</li>
+            <li className={style.sharebtn}><FacebookShareButton url={location.href} >< SiFacebook /> </FacebookShareButton> </li>
+            <li className={style.sharebtn}><LinkedinShareButton url={location.href} >< SiLinkedin /> </LinkedinShareButton> </li>
+            <li className={style.sharebtn}><PinterestShareButton url={location.href} >< SiPinterest /> </PinterestShareButton> </li>
+            <li className={style.sharebtn}><TwitterShareButton url={location.href} >< SiTwitter /> </TwitterShareButton> </li>
         </ul>
     );
 };
