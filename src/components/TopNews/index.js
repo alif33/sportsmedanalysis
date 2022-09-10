441e26b5406b81bd825cfaeec6eb73c5f53f7426
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-09-11 01:21:55 +0600

    design is done

---
+++ src/components/TopNews/index.js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+import style from './TopNews.module.css'
+
+const SimilarNews = () => {
+    return (
+       <div>
+         <div className={style.container}>
+            <div>
+            <Image height="110" width="180" src="/images/card-img/card-img3.png" alt="" />
+            </div>
+            <div>
+                <p className={style.text}>How is the correct solo vlogger</p>
+                <p className={style.description}><small>solo vlogger</small></p>
+                <div className={style.container}>
+                    <p><small className={style.subtitle}>10,0000 listened</small></p>
+                    <p><small className={style.subtitle}>10 Jan 2022</small></p>
+                </div>
+            </div>
+        </div>
+        <p> <small className={style.center}>Show more</small> </p>
+       </div>
+    );
+};
+
+export default SimilarNews;
--- src/section/SingleNews/index.js
+++ src/section/SingleNews/index.js
@@ -1,67 +1,148 @@
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import PostComment from '../../components/PostComment';
-import SimilarNews from '../../components/SimiliarNews';
-import SinglePageComment from '../../components/SinglePageComment';
-import style from './SingleNews.module.css';
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import PostComment from "../../components/PostComment";
+import SimilarNews from "../../components/SimiliarNews";
+import TopNews from "../../components/TopNews";
+import SinglePageComment from "../../components/SinglePageComment";
+import style from "./SingleNews.module.css";
+import single from "../../../pages/[...slug]"
 
 const SingleNews = ({ post, _comments }) => {
-    const [comments, _setComments] = useState();
+  const [comments, _setComments] = useState();
 
-    useEffect(() => {
-        _setComments(_comments)
-    }, [])
-    return (
-        <>
-            <ul className={style.newsCateHour}>
-                <li>{post.league}</li>
-                <li>2 Hours ago</li>
-            </ul>
+  useEffect(() => {
+    _setComments(_comments);
+  }, []);
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style.leftBlock}>
+          <ul className={style.newsCateHour}>
+            <li>{post.league}</li>
+            <li>2 Hours ago</li>
+          </ul>
 
-            <h1 className={style.title}>{post.title}</h1>
-            <div className={style.postMaker}>
-                <Image height="54" width="43" src={post.image} alt={post.title} />
-                <h4>Cameron Williamson</h4>
+          <h1 className={style.title}>{post.title}</h1>
+          <div className={style.postMaker}>
+            <Image height="54" width="43" src={post.image} alt={post.title} />
+            <h4>Cameron Williamson</h4>
+          </div>
 
-            </div>
+          <ul className={style.likeCommentShare}>
+            <li>
+              <Image
+                height="18.73"
+                width="22.47"
+                src="/images/icon/favorite.png"
+                alt=""
+              />{" "}
+              <span>521K</span>
+            </li>
+            <li>
+              <Image
+                height="18.73"
+                width="22.47"
+                src="/images/icon/commnet2.png"
+                alt=""
+              />{" "}
+              <span>521K</span>
+            </li>
+            <li>
+              <Image
+                height="18.73"
+                width="22.47"
+                src="/images/icon/share.png"
+                alt=""
+              />{" "}
+              <span>521K</span>
+            </li>
 
-            <ul className={style.likeCommentShare}>
-                <li><Image height="18.73" width="22.47" src="/images/icon/favorite.png" alt="" /> <span>521K</span></li>
-                <li><Image height="18.73" width="22.47" src="/images/icon/commnet2.png" alt="" /> <span>521K</span></li>
-                <li><Image height="18.73" width="22.47" src="/images/icon/share.png" alt="" /> <span>521K</span></li>
+            <li className="d-md-block d-none">
+              <Image
+                height="20"
+                width="20"
+                src="/images/icon/facebook-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
+            <li className="d-md-none d-block">
+              <Image
+                height="10"
+                width="9"
+                src="/images/icon/facebook-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
 
-                <li className="d-md-block d-none"><Image height="20" width="20" src="/images/icon/facebook-share-icon.png" alt="" /> </li>
-                <li className="d-md-none d-block"><Image height="10" width="9" src="/images/icon/facebook-share-icon.png" alt="" /> </li>
- 
-                <li className="d-md-block d-none"><Image height="20" width="20" src="/images/icon/instagram-share-icon.png" alt="" /> </li>
-                <li className="d-md-none d-block"><Image height="10" width="9" src="/images/icon/instagram-share-icon.png" alt="" /> </li>
+            <li className="d-md-block d-none">
+              <Image
+                height="20"
+                width="20"
+                src="/images/icon/instagram-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
+            <li className="d-md-none d-block">
+              <Image
+                height="10"
+                width="9"
+                src="/images/icon/instagram-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
 
-                <li className="d-md-block d-none"><Image height="20" width="20" src="/images/icon/twitter-share-icon.png" alt="" /> </li>
-                <li className="d-md-none d-block"><Image height="10" width="9" src="/images/icon/twitter-share-icon.png" alt="" /> </li>
-
-            </ul>
-            <div className={style.postImage}>
-                <Image height="593" width="1143" src={post.image} alt={post.title} />
-            </div>
-            <div className={style.descripton}>
-                <p>
-                    {post.description}
-                </p>
-            </div>
-            <PostComment
-                _id={post._id}
-                _setComments={_setComments}
+            <li className="d-md-block d-none">
+              <Image
+                height="20"
+                width="20"
+                src="/images/icon/twitter-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
+            <li className="d-md-none d-block">
+              <Image
+                height="10"
+                width="9"
+                src="/images/icon/twitter-share-icon.png"
+                alt=""
+              />{" "}
+            </li>
+          </ul>
+          <div className={style.postImage}>
+            <Image
+              height="593"
+              width="1143"
+              src={post.image}
+              alt={post.title}
             />
-            {
-                comments && comments.reverse().map((item, index) => <SinglePageComment
-                    key={index}
-                    fullName={item.name}
-                    comment={item.comment}
-                />)
-            }
-            <SimilarNews />
-        </>
-    );
+          </div>
+          <div className={style.descripton}>
+            <p>{post.description}</p>
+          </div>
+          <PostComment _id={post._id} _setComments={_setComments} />
+          {comments &&
+            comments
+              .reverse()
+              .map((item, index) => (
+                <SinglePageComment
+                  key={index}
+                  fullName={item.name}
+                  comment={item.comment}
+                />
+              ))}
+               <p> <small className={style.center}>Load more comments</small> </p>
+          {/* <SimilarNews /> */}
+        </div>
+
+        <div className={style.rightBlock}>
+            <h1 className={style.heading}>Related News</h1>
+            <SimilarNews single={single}/>
+            <h1 className={style.heading}>Top News</h1>
+            <TopNews />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SingleNews;
