e01dd9038684994535930d4c759ad7a5c43c05a7
developerYeasin <yea13sin@gmail.com>
2022-09-14 16:04:06 +0600

    single page

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,89 +1,100 @@
-import React from 'react';
+import React from "react";
 import Post from "../models/Post";
 import db from "../utils/db";
-import Layout from '../src/components/Layout';
-import LiveBtn from '../src/components/LiveBtn';
-import SingleNews from '../src/section/SingleNews';
+import Layout from "../src/components/Layout";
+import LiveBtn from "../src/components/LiveBtn";
+import SingleNews from "../src/section/SingleNews";
 
 const Single = ({ post, posts, topPosts, _comments }) => {
-    return (
-        <Layout>
-            {/* <div className="container-fluid my-2">
-                <LiveBtn name="live" />
-            </div> */}
-            <div className="container">
-                <SingleNews 
-                    post={ post } 
-                    posts={ JSON.parse(posts) }
-                    topPosts={ JSON.parse(topPosts) }
-                    _comments={ JSON.parse(_comments).reverse() }
-                />
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="container">
+        <SingleNews
+          post={post}
+          posts={JSON.parse(posts)}
+          topPosts={JSON.parse(topPosts)}
+          _comments={JSON.parse(_comments).reverse()}
+        />
+      </div>
+    </Layout>
+  );
 };
 
 export default Single;
 
-
 export async function getServerSideProps(context) {
-    const { slug } = context.params;
-    const rgx = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i
-    if(!slug || slug.length !==2 || !rgx.test(slug[1])){
-        return {
-            redirect: {
-                destination: '/404',
-            }
-        }
-    }else{
+  const { slug } = context.params;
+  const rgx = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
+  if (!slug || slug.length !== 2 || !rgx.test(slug[1])) {
+    return {
+      redirect: {
+        destination: "/404",
+      },
+    };
+  } else {
+    await db.connect();
+    const { _doc } = await Post.findById(slug[1]);
+    await Post.updateOne(
+      { _id: slug[1] },
+      {
+        $inc: {
+          views: 1,
+        },
+      }
+    );
 
-        await db.connect();
-        const { _doc } = await Post.findById(slug[1]);
-                    await Post.updateOne({ _id: slug[1] }, 
-                        { $inc: {
-                            "views": 1
-                        }});
+    const {
+      _id,
+      title,
+      image,
+      description,
+      league,
+      playersName,
+      views,
+      tags,
+      comments,
+      _comments,
+      createdAt,
+      updatedAt,
+    } = _doc;
 
-        const { _id, title, image, description, league, playersName, views, tags, comments, _comments, createdAt, updatedAt } = _doc;
+    const posts = await Post.find(
+      {
+        tags: { $in: [...tags] },
+      },
+      { _comments: 0 }
+    )
+      .sort({ createdAt: -1 })
+      .lean()
+      .limit(5);
 
-        const posts = await Post.find({
-            tags: { $in: [...tags] }
-        }, { _comments: 0 })
-            .sort({"createdAt": -1})
-            .lean()
-            .limit(5);
+    const topPosts = await Post.find({}, { _comments: 0 })
+      .sort({ views: -1 })
+      .lean()
+      .limit(5);
 
-        const topPosts = await Post.find({}, { _comments: 0 })
-            .sort({ "views": -1 })
-            .lean()
-            .limit(5);
+    await db.disconnect();
 
+    // console.log(posts);
 
-        await db.disconnect();
-        
-        // console.log(posts);
-        
-        return {
-            props: {
-                post :  {
-                    _id: _id.toString(),
-                    title, 
-                    image, 
-                    description, 
-                    league, 
-                    playersName, 
-                    views, 
-                    tags,
-                    createdAt: createdAt.toString(),
-                    updatedAt: updatedAt.toString()
-                },
-                posts: JSON.stringify(posts),
-                topPosts: JSON.stringify(topPosts),
-                _comments: JSON.stringify(_comments),
-            }
-        };
-    }
+    return {
+      props: {
+        post: {
+          _id: _id.toString(),
+          title,
+          image,
+          description,
+          league,
+          playersName,
+          views,
+          tags,
+          createdAt: createdAt.toString(),
+          updatedAt: updatedAt.toString(),
+        },
+        posts: JSON.stringify(posts),
+        topPosts: JSON.stringify(topPosts),
+        _comments: JSON.stringify(_comments),
+      },
+    };
+  }
 }
-
-
-
--- src/section/SingleNews/index.js
+++ src/section/SingleNews/index.js
@@ -23,17 +23,15 @@ const SingleNews = ({ post, posts, topPosts, _comments }) => {
     <>
       <div className="row mt-5">
         <div className="col-md-8">
-          <h2 className={style.categoryName}>NFL News</h2>
+          <h1 className={style.title}>{post.title}</h1>
           <ul className={style.newsCateHour}>
             <li>{post.league}</li>
             <li>2 Hours ago</li>
           </ul>
-
-          <h1 className={style.title}>{post.title}</h1>
-          <div className={style.postMaker}>
+          {/* <div className={style.postMaker}>
             <Image height="250" width="250" src={post.image} alt={post.title} />
             <h4>Cameron Williamson</h4>
-          </div>
+          </div> */}
 
           <ul className={style.likeCommentShare}>
             <li>
@@ -46,57 +44,6 @@ const SingleNews = ({ post, posts, topPosts, _comments }) => {
             </li>
             <li>
               <ShareModal />
-            </li>
-
-            <li className="d-md-block d-none">
-              <Image
-                height="20"
-                width="20"
-                src="/images/icon/facebook-share-icon.png"
-                alt=""
-              />{" "}
-            </li>
-            <li className="d-md-none d-block">
-              <Image
-                height="10"
-                width="9"
-                src="/images/icon/facebook-share-icon.png"
-                alt=""
-              />{" "}
-            </li>
-
-            <li className="d-md-block d-none">
-              <Image
-                height="20"
-                width="20"
-                src="/images/icon/instagram-share-icon.png"
-                alt=""
-              />{" "}
-            </li>
-            <li className="d-md-none d-block">
-              <Image
-                height="10"
-                width="9"
-                src="/images/icon/instagram-share-icon.png"
-                alt=""
-              />{" "}
-            </li>
-
-            <li className="d-md-block d-none">
-              <Image
-                height="20"
-                width="20"
-                src="/images/icon/twitter-share-icon.png"
-                alt=""
-              />{" "}
-            </li>
-            <li className="d-md-none d-block">
-              <Image
-                height="10"
-                width="9"
-                src="/images/icon/twitter-share-icon.png"
-                alt=""
-              />{" "}
             </li>
           </ul>
           <div className={style.postImage}>
@@ -131,20 +78,13 @@ const SingleNews = ({ post, posts, topPosts, _comments }) => {
 
         <div className="col-md-4">
           <h1 className={style.heading}>Related News</h1>
-          { posts && posts.map((item, index)=><SimilarNews 
-              key={ index }
-              post={ item }
-            />
-          )}
+          {posts &&
+            posts.map((item, index) => <SimilarNews key={index} post={item} />)}
           {/* <SimilarNews single={single} />
           <SimilarNews single={single} /> */}
           <h1 className={style.heading2}>Top News</h1>
-          {
-            topPosts && topPosts.map((item, index)=><TopNews 
-              key={ index }
-              post={ item }
-            />)
-          }
+          {topPosts &&
+            topPosts.map((item, index) => <TopNews key={index} post={item} />)}
         </div>
       </div>
     </>
