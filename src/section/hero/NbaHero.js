96f536bf2b9a4e1bcce56344daf533caf2de4f8c
developerYeasin <yea13sin@gmail.com>
2022-09-16 00:09:39 +0600

    lsadkjf;

--- src/section/hero/NbaHero.js
+++ src/section/hero/NbaHero.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import { imageResize, Sortern } from "../../../__lib__/helpers/Validator";
 import style from "./NbaHero.module.css";
 
 const NbaHero = ({ posts }) => {
   console.log(posts);
-  const _image = posts[1].image.split("upload");
+  // const _image = posts[1].image.split("upload");
   const _image2 = posts[2].image.split("upload");
   const _image3 = posts[3].image.split("upload");
 
@@ -13,14 +14,14 @@ const NbaHero = ({ posts }) => {
       <div className={style._smd_nbahero_card_main}>
         <Image
           //   src="/images/nba/header-img1.png"
-          src={`${_image[0]}/upload/c_thumb,g_face,h_844,w_1000/${_image[1]}`}
+          src={imageResize(posts[5].image, "c_thumb,g_face,h_844,w_1000")}
           alt=""
           height="418"
           width="500"
         />
         <Link href={`/${posts[1].slug}/${posts[1]._id}`}>
           <a>
-            <h2>{posts[1].title.slice(0, 40)}</h2>
+            <h2>{Sortern(posts[1].title, 6)}</h2>
           </a>
         </Link>
       </div>
@@ -28,28 +29,28 @@ const NbaHero = ({ posts }) => {
         <div className={style._smd_nbahero_card}>
           <Image
             // src="/images/nba/header-card-img.png"
-            src={`${_image2[0]}/upload/c_thumb,g_face,h_416,w_1000/${_image2[1]}`}
+            src={imageResize(posts[6].image, "c_thumb,g_face,h_416,w_1000")}
             alt=""
             height="209"
             width="500"
           />
-          <Link href={`/${posts[2].slug}/${posts[2]._id}`}>
+          <Link href={`/${posts[6].slug}/${posts[6]._id}`}>
             <a>
-              <h2>{posts[2].title.slice(0, 40)}</h2>
+              <h2>{Sortern(posts[6].title, 6)}</h2>
             </a>
           </Link>
         </div>
         <div className={style._smd_nbahero_card}>
           <Image
             // src="/images/nba/header-card-img1.png"
-            src={`${_image3[0]}/upload/c_thumb,g_face,h_416,w_1000/${_image3[1]}`}
+            src={imageResize(posts[2].image, "c_thumb,g_face,h_416,w_1000")}
             alt=""
             height="209"
             width="500"
           />
-          <Link href={`/${posts[3].slug}/${posts[3]._id}`}>
+          <Link href={`/${posts[2].slug}/${posts[2]._id}`}>
             <a>
-              <h2>{posts[3].title.slice(0, 40)}</h2>
+              <h2>{Sortern(posts[2].title, 6)}</h2>
             </a>
           </Link>
         </div>
