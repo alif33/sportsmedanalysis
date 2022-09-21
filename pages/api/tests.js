270643f1cc256485af0e2d1388470934f1684df5
alif33 <alifhasan332@gmail.com>
2022-09-21 14:31:39 +0600

    ...

--- pages/api/tests.js
+++ pages/api/tests.js
@@ -1,16 +1,17 @@
 import nc from 'next-connect';
 import db from '../../utils/db';
 import Player from '../../models/Player';
+import Post from '../../models/Post';
 
 const handler = nc();
 
 handler.get(async (req, res) => {
     await db.connect();
-    const players = await Player.find();
+    const players = await Post.find();
     await db.disconnect();
     players.forEach(item=>{
         const { _id, firstName, lastName } = item;
-        Player.findOneAndUpdate(
+        Post.findOneAndUpdate(
             { _id }, 
             { $set: {
                 slug: firstName+"-"+ lastName
