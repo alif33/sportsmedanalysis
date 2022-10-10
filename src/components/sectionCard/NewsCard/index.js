fec6c0463f22fbe74507efbc2a2a34f42054ec5b
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2022-10-10 19:30:09 +0600

    ...

--- src/components/sectionCard/NewsCard/index.js
+++ src/components/sectionCard/NewsCard/index.js
@@ -2,22 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 import { imageResize, Sortern } from "../../../../__lib__/helpers/Validator";
 import style from "./NewsCard.module.css";
-
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 const NewsCard = ({ _id, slug, title, img, description }) => {
-  console.log(img);
+  console.log("isMobileisMobile", isMobile);
   return (
     <Link href={`/${slug}/${_id}`}>
       <a className={style.news_card}>
-        <Image
-          src={
-            img
-              ? imageResize(img, "c_thumb,g_face,h_930,w_1480")
-              : "/images/card-img/card-img2.png"
-          }
-          alt={title}
-          height="900"
-          width="880"
-        />
+        <div className={style._news_card_desk}>
+          <Image
+            src={
+              img
+                ? imageResize(img, "c_thumb,g_face,h_930,w_1480")
+                : "/images/card-img/card-img2.png"
+            }
+            alt={title}
+            height="465"
+            width="880"
+          />{" "}
+        </div>
+
+        <div className={style._news_card_mobile}>
+          <Image
+            src={
+              img
+                ? imageResize(img, "c_thumb,h_460,w_732")
+                : "/images/card-img/card-img2.png"
+            }
+            alt={title}
+            height="230"
+            width="366"
+          />
+        </div>
         <h3>{Sortern(title, 15)}</h3>
         <p>{Sortern(description, 30)}</p>
       </a>
