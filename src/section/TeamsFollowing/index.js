9fb916d25dcd8712ff3f2ba557ff5f2d32b23ca1
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2022-10-10 15:03:54 +0600

    ...

--- src/section/TeamsFollowing/index.js
+++ src/section/TeamsFollowing/index.js
@@ -7,7 +7,7 @@ import style from "./TeamsFollowing.module.css";
 const TeamsFollowing = ({ title, teams }) => {
   return (
     <>
-      <div className="d-flex justify-content-between mt-3">
+      <div className="d-flex justify-content-between mt-3 mx-1">
         <h2 className={style.teamsFollowingTitle}>{title}</h2>
         <div className={style.TeamsFollowingSort}>
           <SearchBar />
@@ -20,19 +20,21 @@ const TeamsFollowing = ({ title, teams }) => {
           />
         </div>
       </div>
-      {teams &&
-        teams.map((team, index) => {
-          return (
-            <TeamsFollowingCard
-              key={index}
-              _id={team._id}
-              name={team.name}
-              league={team.league}
-            />
-          );
-        })}
+      <div className="mx-1">
+        {teams &&
+          teams.map((team, index) => {
+            return (
+              <TeamsFollowingCard
+                key={index}
+                _id={team._id}
+                name={team.name}
+                league={team.league}
+              />
+            );
+          })}
+      </div>
 
-      <div className="d-flex justify-content-between align-items-center">
+      <div className="d-flex justify-content-between align-items-center mx-1">
         <button className={style.nextPreBtn}>Previous Page</button>
         <button className={style.nextPreBtn}>Next Page</button>
       </div>
